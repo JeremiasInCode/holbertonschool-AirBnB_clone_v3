09e3ec6655d8086d6fb67ca647dfed9d281e082f
JereDev19 <jeremiaserba7894@gmail.com>
2023-12-19 18:20:52 -0300

    Task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,7 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
-from flasgger import Swagger
-from flasgger.utils import swag_from
+from flask import Flask
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
